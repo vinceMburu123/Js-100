bdcf0bfab633fe76554b4059e0b5cfdbc620796e
vinceMburu123 <judevinny@gmail.com>
2024-10-01 07:41:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const  body =document.body
+const div = document.createElement("div")
+const strong = document.createElement("strong")
+const spanHi =document.querySelector("#hi")
+const spanBye =document.querySelector("#Bye")
+
+spanHi.style.backgroundColor="blue"
+
